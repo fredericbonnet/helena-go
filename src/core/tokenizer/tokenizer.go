4a788ec681f084c451f2490b6173de402a45b9c6
Frédéric Bonnet <frederic.bonnet@free.fr>
2024-01-28 12:17:53 +0100

    Convert TS tokenizer to Go

---
+++ src/core/tokenizer/tokenizer.go
@@ -0,0 +1,528 @@
+// Helena tokenization
+package tokenizer
+
+//
+// Helena token type for each special character or sequence
+//
+type TokenType int8
+
+const (
+	WHITESPACE TokenType = iota
+	NEWLINE
+	CONTINUATION
+	TEXT
+	ESCAPE
+	COMMENT
+	OPEN_TUPLE
+	CLOSE_TUPLE
+	OPEN_BLOCK
+	CLOSE_BLOCK
+	OPEN_EXPRESSION
+	CLOSE_EXPRESSION
+	STRING_DELIMITER
+	DOLLAR
+	SEMICOLON
+	ASTERISK
+)
+
+//
+// Position in source stream
+//
+type SourcePosition struct {
+	// Character Index (zero-indexed)
+	Index uint
+
+	// Line number (zero-indexed)
+	Line uint
+
+	// Column number (zero-indexed)
+	Column uint
+}
+
+// Advance to Next character.
+//
+// If newline is true, increment line number as well.
+//
+// Returns the previous index.
+func (pos *SourcePosition) Next(newline bool) uint {
+	if newline {
+		pos.Line++
+		pos.Column = 0
+	} else {
+		pos.Column++
+	}
+	pos.Index += 1
+	return pos.Index - 1
+}
+
+//
+// Helena token
+//
+type Token struct {
+	// Token type
+	Type TokenType
+
+	// Position in source stream
+	Position SourcePosition
+
+	// Raw Sequence of characters from stream
+	Sequence string
+
+	// String Literal
+	Literal string
+}
+
+// Helena tokenizer
+//
+// This class transforms a stream of characters into a stream of tokens
+type Tokenizer struct {
+	// Input stream
+	input SourceStream
+
+	// Current token
+	currentToken *Token
+}
+
+// Tokenize and return a Helena source string into a token array
+func (tokenizer Tokenizer) Tokenize(source string) []Token {
+	input := NewStringStream(source)
+	output := NewArrayTokenStream([]Token{})
+	tokenizer.TokenizeStream(&input, &output)
+	return output.tokens
+}
+
+// Tokenize the Helena input source stream into the output token stream
+func (tokenizer *Tokenizer) TokenizeStream(input SourceStream, output TokenStream) {
+	tokenizer.Begin(input)
+	for !tokenizer.End() {
+		emittedToken := tokenizer.Next()
+		if emittedToken != nil {
+			output.emit(*emittedToken)
+		}
+	}
+}
+
+// Start incremental tokenization of a Helena input source stream
+func (tokenizer *Tokenizer) Begin(input SourceStream) {
+	tokenizer.input = input
+	tokenizer.currentToken = nil
+}
+
+// Report whether tokenization is done
+func (tokenizer *Tokenizer) End() bool {
+	return tokenizer.input.end() && (tokenizer.currentToken == nil)
+}
+
+// Return current token and advance to next one
+func (tokenizer *Tokenizer) Next() *Token {
+	for !tokenizer.input.end() {
+		position := tokenizer.input.currentPosition()
+		c := tokenizer.input.next()
+		var emittedToken *Token
+		switch c {
+		// Whitespaces
+		case ' ',
+			'\t',
+			'\r',
+			'\f':
+			for !tokenizer.input.end() && isWhitespace(tokenizer.input.current()) {
+				tokenizer.input.next()
+			}
+			emittedToken = tokenizer.addToken(WHITESPACE, position, nil)
+
+		// Newline
+		case '\n':
+			emittedToken = tokenizer.addToken(NEWLINE, position, nil)
+
+		// Escape sequence
+		case '\\':
+			{
+				if tokenizer.input.end() {
+					emittedToken = tokenizer.addToken(TEXT, position, nil)
+					break
+				}
+				e := tokenizer.input.next()
+				if e == '\n' {
+					// Continuation, eat up all subsequent whitespaces
+					for !tokenizer.input.end() && isWhitespace(tokenizer.input.current()) {
+						tokenizer.input.next()
+					}
+					literal := " "
+					emittedToken = tokenizer.addToken(CONTINUATION, position, &literal)
+					break
+				}
+				escape := string(e) // Default value for unrecognized sequences
+				if isEscape(e) {
+					escape = string(getEscape(e))
+				} else if isOctal(e) {
+					codepoint := rune(e - '0')
+					for i := 1; !tokenizer.input.end() &&
+						isOctal(tokenizer.input.current()) &&
+						i < 3; i += 1 {
+						codepoint *= 8
+						codepoint += rune(digitValue(tokenizer.input.current()))
+						tokenizer.input.next()
+					}
+					escape = string(rune(codepoint))
+				} else if e == 'x' {
+					codepoint := rune(0)
+					i := 0
+					for ; !tokenizer.input.end() &&
+						isHexadecimal(tokenizer.input.current()) &&
+						i < 2; i += 1 {
+						codepoint *= 16
+						codepoint += rune(digitValue(tokenizer.input.current()))
+						tokenizer.input.next()
+					}
+					if i > 0 {
+						escape = string(rune(codepoint))
+					}
+				} else if e == 'u' {
+					codepoint := rune(0)
+					i := 0
+					for ; !tokenizer.input.end() &&
+						isHexadecimal(tokenizer.input.current()) &&
+						i < 4; i += 1 {
+						codepoint *= 16
+						codepoint += rune(digitValue(tokenizer.input.current()))
+						tokenizer.input.next()
+					}
+					if i > 0 {
+						escape = string(rune(codepoint))
+					}
+				} else if e == 'U' {
+					codepoint := rune(0)
+					i := 0
+					for ; !tokenizer.input.end() &&
+						isHexadecimal(tokenizer.input.current()) &&
+						i < 8; i += 1 {
+						codepoint *= 16
+						codepoint += rune(digitValue(tokenizer.input.current()))
+						tokenizer.input.next()
+					}
+					if i > 0 {
+						escape = string(rune(codepoint))
+					}
+				}
+				emittedToken = tokenizer.addToken(ESCAPE, position, &escape)
+			}
+
+		// Comment
+		case '#':
+			for !tokenizer.input.end() && tokenizer.input.current() == '#' {
+				tokenizer.input.next()
+			}
+			emittedToken = tokenizer.addToken(COMMENT, position, nil)
+
+		// Tuple delimiters
+		case '(':
+			emittedToken = tokenizer.addToken(OPEN_TUPLE, position, nil)
+
+		case ')':
+			emittedToken = tokenizer.addToken(CLOSE_TUPLE, position, nil)
+
+		// Block delimiters
+		case '{':
+			emittedToken = tokenizer.addToken(OPEN_BLOCK, position, nil)
+
+		case '}':
+			emittedToken = tokenizer.addToken(CLOSE_BLOCK, position, nil)
+
+		// Expression delimiters
+		case '[':
+			emittedToken = tokenizer.addToken(OPEN_EXPRESSION, position, nil)
+
+		case ']':
+			emittedToken = tokenizer.addToken(CLOSE_EXPRESSION, position, nil)
+
+		// String delimiter
+		case '"':
+			for !tokenizer.input.end() && tokenizer.input.current() == '"' {
+				tokenizer.input.next()
+			}
+			emittedToken = tokenizer.addToken(STRING_DELIMITER, position, nil)
+
+		// Dollar
+		case '$':
+			emittedToken = tokenizer.addToken(DOLLAR, position, nil)
+
+		// Semicolon
+		case ';':
+			emittedToken = tokenizer.addToken(SEMICOLON, position, nil)
+
+		// Asterisk
+		case '*':
+			emittedToken = tokenizer.addToken(ASTERISK, position, nil)
+
+		default:
+			emittedToken = tokenizer.addText(position)
+		}
+		if emittedToken != nil {
+			return emittedToken
+		}
+	}
+	return tokenizer.emitToken()
+}
+
+// Emit and return current token or nil
+func (tokenizer *Tokenizer) emitToken() *Token {
+	emitted := tokenizer.currentToken
+	tokenizer.currentToken = nil
+	return emitted
+}
+
+// Add and return token of given type and starting at position to result.
+//
+// The token can have an optional literal value.
+func (tokenizer *Tokenizer) addToken(
+	type_ TokenType,
+	position SourcePosition,
+	literal *string,
+) *Token {
+	emitted := tokenizer.emitToken()
+	sequence := tokenizer.input.range_(
+		position.Index,
+		tokenizer.input.currentIndex(),
+	)
+	var lit string
+	if literal != nil {
+		lit = *literal
+	} else {
+		lit = sequence
+	}
+	tokenizer.currentToken = &Token{
+		type_,
+		position,
+		sequence,
+		lit,
+	}
+	return emitted
+}
+
+// Add character sequence to new or existing text token
+//
+// Added character sequence is between given position and current stream
+// position
+func (tokenizer *Tokenizer) addText(position SourcePosition) *Token {
+	literal := tokenizer.input.range_(position.Index, tokenizer.input.currentIndex())
+	if tokenizer.currentToken == nil || tokenizer.currentToken.Type != TEXT {
+		return tokenizer.addToken(TEXT, position, &literal)
+	} else {
+		tokenizer.currentToken.Literal += literal
+		tokenizer.currentToken.Sequence = tokenizer.input.range_(
+			tokenizer.currentToken.Position.Index,
+			tokenizer.input.currentIndex(),
+		)
+		return nil
+	}
+}
+
+// Report whether character c is a whitespace (excluding newlines)
+func isWhitespace(c byte) bool {
+	switch c {
+	case ' ', '\t', '\r', '\x0C':
+		return true
+	default:
+		return false
+	}
+}
+
+// Report whether character c is a known escape
+func isEscape(c byte) bool {
+	switch c {
+	case 'a', 'b', 'f', 'n', 'r', 't', 'v', '\\':
+		return true
+	default:
+		return false
+	}
+}
+
+// Return escaped rune from character c
+func getEscape(c byte) rune {
+	switch c {
+	case 'a':
+		return '\x07'
+	case 'b':
+		return '\b'
+	case 'f':
+		return '\f'
+	case 'n':
+		return '\n'
+	case 'r':
+		return '\r'
+	case 't':
+		return '\t'
+	case 'v':
+		return '\v'
+	case '\\':
+		return '\\'
+	default:
+		panic("unreachable")
+	}
+}
+
+// Report whether character c is octal
+func isOctal(c byte) bool {
+	return (c >= '0' && c <= '7')
+}
+
+// Report whether character c is hexadecimal
+func isHexadecimal(c byte) bool {
+	return (c >= '0' && c <= '9') ||
+		(c >= 'a' && c <= 'f') ||
+		(c >= 'A' && c <= 'F')
+}
+
+/// Get digit value from character c
+func digitValue(c byte) rune {
+	switch {
+	case c >= '0' && c <= '9':
+		return rune(c - '0')
+	case c >= 'a' && c <= 'f':
+		return rune(c - 'a' + 10)
+	case c >= 'A' && c <= 'F':
+		return rune(c - 'A' + 10)
+	default:
+		panic("unreachable")
+	}
+}
+
+//
+// Source stream (input)
+//
+type SourceStream interface {
+	// Report whether stream is at end
+	end() bool
+
+	// Advance to next character and return character at previous position
+	next() byte
+
+	// Get current character
+	current() byte
+
+	// Get range of characters between start (inclusive) and end (exclusive)
+	range_(start uint, end uint) string
+
+	// Get current character index
+	currentIndex() uint
+
+	// Get current character position
+	currentPosition() SourcePosition
+}
+
+//
+// String-based character stream
+//
+type StringStream struct {
+	// Source string
+	source string
+
+	// Current input position in stream
+	position SourcePosition
+}
+
+// Create a new stream from the source string
+func NewStringStream(source string) StringStream {
+	return StringStream{source, SourcePosition{}}
+}
+
+// Report whether stream is at end
+func (stream *StringStream) end() bool {
+	return stream.position.Index >= uint(len(stream.source))
+}
+
+// Advance to next character and return character at previous position
+func (stream *StringStream) next() byte {
+	return stream.source[stream.position.Next(stream.current() == '\n')]
+}
+
+// Get current character
+func (stream *StringStream) current() byte {
+	return stream.source[stream.position.Index]
+}
+
+// Get range of characters between start (inclusive) and end (exclusive)
+func (stream *StringStream) range_(start uint, end uint) string {
+
+	return stream.source[start:end]
+}
+
+// Get current character index
+func (stream *StringStream) currentIndex() uint {
+	return stream.position.Index
+}
+
+// Get current character position
+func (stream *StringStream) currentPosition() SourcePosition {
+	return stream.position
+}
+
+//
+// Token stream (input/output)
+//
+type TokenStream interface {
+	// Emit (add) token to end of stream
+	emit(token Token)
+
+	// Reports whether stream is at end
+	end() bool
+
+	// Advance to next token and return token at previous position
+	next() Token
+
+	// Get current token
+	current() Token
+
+	// Get range of tokens between start (inclusive) and end (exclusive)
+	range_(start uint, end uint) []Token
+
+	// Get current token index
+	currentIndex() uint
+}
+
+//
+// Array-based token stream
+//
+type ArrayTokenStream struct {
+	// Emitted tokens
+	tokens []Token
+
+	// Current input position in stream
+	index uint
+}
+
+// Create a new stream from from the tokens array
+func NewArrayTokenStream(tokens []Token) ArrayTokenStream {
+	return ArrayTokenStream{tokens, 0}
+}
+
+// Emit (add) token to end of stream
+func (stream *ArrayTokenStream) emit(token Token) {
+	stream.tokens = append(stream.tokens, token)
+}
+
+// Reports whether stream is at end
+func (stream *ArrayTokenStream) end() bool {
+	return stream.index >= uint(len(stream.tokens))
+}
+
+// Advance to next token and return token at previous position
+func (stream *ArrayTokenStream) next() Token {
+	stream.index += 1
+	return stream.tokens[stream.index-1]
+}
+
+// Get current token
+func (stream *ArrayTokenStream) current() Token {
+	return stream.tokens[stream.index]
+}
+
+// Get range of tokens between start (inclusive) and end (exclusive)
+func (stream *ArrayTokenStream) range_(start uint, end uint) []Token {
+	return stream.tokens[start:end]
+}
+
+// Get current token index
+func (stream *ArrayTokenStream) currentIndex() uint {
+	return stream.index
+}
---
+++ src/core/tokenizer/tokenizer_suite_test.go
@@ -0,0 +1,455 @@
+package tokenizer_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	. "helena/core/tokenizer"
+)
+
+func TestTokenizer(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Tokenizer Suite")
+}
+
+func toType(token Token) TokenType  { return token.Type }
+func toIndex(token Token) uint      { return token.Position.Index }
+func toLine(token Token) uint       { return token.Position.Line }
+func toColumn(token Token) uint     { return token.Position.Column }
+func toLiteral(token Token) string  { return token.Literal }
+func toSequence(token Token) string { return token.Sequence }
+
+func mapToType(tokens []Token) []TokenType {
+	result := make([]TokenType, len(tokens))
+	for i, token := range tokens {
+		result[i] = toType(token)
+	}
+	return result
+}
+func mapToLiteral(tokens []Token) []string {
+	result := make([]string, len(tokens))
+	for i, token := range tokens {
+		result[i] = toLiteral(token)
+	}
+	return result
+}
+func mapToIndex(tokens []Token) []uint {
+	result := make([]uint, len(tokens))
+	for i, token := range tokens {
+		result[i] = toIndex(token)
+	}
+	return result
+}
+func mapToLine(tokens []Token) []uint {
+	result := make([]uint, len(tokens))
+	for i, token := range tokens {
+		result[i] = toLine(token)
+	}
+	return result
+}
+func mapToColumn(tokens []Token) []uint {
+	result := make([]uint, len(tokens))
+	for i, token := range tokens {
+		result[i] = toColumn(token)
+	}
+	return result
+}
+func mapToSequence(tokens []Token) []string {
+	result := make([]string, len(tokens))
+	for i, token := range tokens {
+		result[i] = toSequence(token)
+	}
+	return result
+}
+
+var _ = Describe("Tokenizer", func() {
+	var tokenizer Tokenizer
+	BeforeEach(func() {
+		tokenizer = Tokenizer{}
+	})
+
+	Specify("empty string", func() {
+		Expect(tokenizer.Tokenize("")).To(BeEmpty())
+	})
+
+	Describe("types", func() {
+		Specify("whitespace", func() {
+			Expect(mapToType(tokenizer.Tokenize(" "))).To(Equal([]TokenType{
+				WHITESPACE,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("\t"))).To(Equal([]TokenType{
+				WHITESPACE,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("\r"))).To(Equal([]TokenType{
+				WHITESPACE,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("\f"))).To(Equal([]TokenType{
+				WHITESPACE,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("  "))).To(Equal([]TokenType{
+				WHITESPACE,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("   \t\f  \r\r "))).To(Equal([]TokenType{
+				WHITESPACE,
+			}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\ "))).To(Equal([]TokenType{ESCAPE}))
+			Expect(mapToType(tokenizer.Tokenize("\\\t"))).To(Equal([]TokenType{ESCAPE}))
+			Expect(mapToType(tokenizer.Tokenize("\\\r"))).To(Equal([]TokenType{ESCAPE}))
+			Expect(mapToType(tokenizer.Tokenize("\\\f"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("newline", func() {
+			Expect(mapToType(tokenizer.Tokenize("\n"))).To(Equal([]TokenType{NEWLINE}))
+			Expect(mapToType(tokenizer.Tokenize("\n\n"))).To(Equal([]TokenType{
+				NEWLINE,
+				NEWLINE,
+			}))
+		})
+
+		Describe("escape sequences", func() {
+			Specify("backslash", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\"))).To(Equal([]TokenType{TEXT}))
+			})
+			Specify("continuation", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\\n"))).To(Equal([]TokenType{
+					CONTINUATION,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\\n   "))).To(Equal([]TokenType{
+					CONTINUATION,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\\n \t\r\f "))).To(Equal([]TokenType{
+					CONTINUATION,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\\n \t \\\n  "))).To(Equal([]TokenType{
+					CONTINUATION,
+					CONTINUATION,
+				}))
+			})
+			Specify("control characters", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\a"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\b"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\f"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\n"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\r"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\t"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\v"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\\\"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+			})
+			Specify("octal sequence", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\1"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\123"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\1234"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\0x"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+			})
+			Specify("hexadecimal sequence", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\x1"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\x12"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\x123"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\x1f"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\x1F"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\x1g"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+			})
+			Specify("unicode sequence", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\u1"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U1"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\u12"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U12"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\u123456"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U12345"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U0123456789"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\u1f"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\u1F"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U1f"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U1F"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\u1g"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U1g"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+			})
+			Specify("unrecognized sequences", func() {
+				Expect(mapToType(tokenizer.Tokenize("\\8"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\9"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\c"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\d"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\e"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\x"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\xg"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\u"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\ug"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\U"))).To(Equal([]TokenType{
+					ESCAPE,
+				}))
+				Expect(mapToType(tokenizer.Tokenize("\\Ug"))).To(Equal([]TokenType{
+					ESCAPE,
+					TEXT,
+				}))
+			})
+		})
+
+		Specify("comments", func() {
+			Expect(mapToType(tokenizer.Tokenize("#"))).To(Equal([]TokenType{COMMENT}))
+			Expect(mapToType(tokenizer.Tokenize("###"))).To(Equal([]TokenType{COMMENT}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\#"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("tuples", func() {
+			Expect(mapToType(tokenizer.Tokenize("("))).To(Equal([]TokenType{
+				OPEN_TUPLE,
+			}))
+			Expect(mapToType(tokenizer.Tokenize(")"))).To(Equal([]TokenType{
+				CLOSE_TUPLE,
+			}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\("))).To(Equal([]TokenType{ESCAPE}))
+			Expect(mapToType(tokenizer.Tokenize("\\)"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("blocks", func() {
+			Expect(mapToType(tokenizer.Tokenize("{"))).To(Equal([]TokenType{
+				OPEN_BLOCK,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("}"))).To(Equal([]TokenType{
+				CLOSE_BLOCK,
+			}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\{"))).To(Equal([]TokenType{ESCAPE}))
+			Expect(mapToType(tokenizer.Tokenize("\\}"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("expressions", func() {
+			Expect(mapToType(tokenizer.Tokenize("["))).To(Equal([]TokenType{
+				OPEN_EXPRESSION,
+			}))
+			Expect(mapToType(tokenizer.Tokenize("]"))).To(Equal([]TokenType{
+				CLOSE_EXPRESSION,
+			}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\["))).To(Equal([]TokenType{ESCAPE}))
+			Expect(mapToType(tokenizer.Tokenize("\\]"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("strings", func() {
+			Expect(mapToType(tokenizer.Tokenize(`"`))).To(Equal([]TokenType{
+				STRING_DELIMITER,
+			}))
+
+			Expect(mapToType(tokenizer.Tokenize(`\"`))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("dollar", func() {
+			Expect(mapToType(tokenizer.Tokenize("$"))).To(Equal([]TokenType{DOLLAR}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\$"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("semicolon", func() {
+			Expect(mapToType(tokenizer.Tokenize(";"))).To(Equal([]TokenType{SEMICOLON}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\;"))).To(Equal([]TokenType{ESCAPE}))
+		})
+
+		Specify("asterisk", func() {
+			Expect(mapToType(tokenizer.Tokenize("*"))).To(Equal([]TokenType{ASTERISK}))
+
+			Expect(mapToType(tokenizer.Tokenize("\\*"))).To(Equal([]TokenType{ESCAPE}))
+		})
+	})
+
+	Describe("positions", func() {
+		It("should track index", func() {
+			Expect(mapToIndex(tokenizer.Tokenize("a b c"))).To(Equal([]uint{0, 1, 2, 3, 4}))
+			Expect(mapToIndex(tokenizer.Tokenize("abc \r\f de\tf"))).To(Equal([]uint{
+				0, 3, 7, 9, 10,
+			}))
+		})
+		It("should track line", func() {
+			Expect(mapToLine(tokenizer.Tokenize("a b c"))).To(Equal([]uint{0, 0, 0, 0, 0}))
+			Expect(mapToLine(tokenizer.Tokenize("a\nbcd e\nfg  h"))).To(Equal([]uint{
+				0, 0, 1, 1, 1, 1, 2, 2, 2,
+			}))
+		})
+		It("should track column", func() {
+			Expect(mapToColumn(tokenizer.Tokenize("a b c"))).To(Equal([]uint{0, 1, 2, 3, 4}))
+			Expect(mapToColumn(tokenizer.Tokenize("a\nbcd e\nfg  h"))).To(Equal([]uint{
+				0, 1, 0, 3, 4, 5, 0, 2, 4,
+			}))
+		})
+	})
+
+	Describe("literals", func() {
+		Specify("text", func() {
+			Expect(mapToLiteral(tokenizer.Tokenize("abcd"))).To(Equal([]string{"abcd"}))
+		})
+		Specify("escape", func() {
+			Expect(
+				mapToLiteral(tokenizer.Tokenize("\\a\\b\\f\\n\\r\\t\\v\\\\")),
+			).To(Equal([]string{
+				"\x07", "\b", "\f", "\n", "\r", "\t", "\v", "\\",
+			}))
+			Expect(mapToLiteral(tokenizer.Tokenize("\\123"))).To(Equal([]string{"S"}))
+			Expect(mapToLiteral(tokenizer.Tokenize("\\xA5"))).To(Equal([]string{"¥"}))
+			Expect(mapToLiteral(tokenizer.Tokenize("\\u1234"))).To(Equal([]string{"\u1234"}))
+			Expect(
+				mapToLiteral(tokenizer.Tokenize("\\U00012345\\U0006789A")),
+			).To(Equal([]string{string(rune(0x12345)), string(rune(0x6789a))}))
+			Expect(
+				mapToLiteral(tokenizer.Tokenize("\\8\\9\\c\\d\\e\\x\\xg\\u\\ug\\U\\Ug")),
+			).To(Equal([]string{
+				"8",
+				"9",
+				"c",
+				"d",
+				"e",
+				"x",
+				"x",
+				"g",
+				"u",
+				"u",
+				"g",
+				"U",
+				"U",
+				"g",
+			}))
+		})
+		Specify("continuation", func() {
+			Expect(mapToLiteral(tokenizer.Tokenize("\\\n"))).To(Equal([]string{" "}))
+			Expect(mapToLiteral(tokenizer.Tokenize("\\\n   "))).To(Equal([]string{" "}))
+			Expect(mapToLiteral(tokenizer.Tokenize("\\\n \t\r\f "))).To(Equal([]string{" "}))
+			Expect(mapToLiteral(tokenizer.Tokenize("\\\n \t \\\n  "))).To(Equal([]string{
+				" ",
+				" ",
+			}))
+		})
+	})
+	Describe("sequences", func() {
+		Specify("text", func() {
+			Expect(mapToSequence(tokenizer.Tokenize("abcd"))).To(Equal([]string{"abcd"}))
+		})
+		Specify("escape", func() {
+			Expect(
+				mapToSequence(tokenizer.Tokenize("\\a\\b\\f\\n\\r\\t\\v\\\\")),
+			).To(Equal([]string{"\\a", "\\b", "\\f", "\\n", "\\r", "\\t", "\\v", "\\\\"}))
+			Expect(mapToSequence(tokenizer.Tokenize("\\123"))).To(Equal([]string{"\\123"}))
+			Expect(mapToSequence(tokenizer.Tokenize("\\xA5"))).To(Equal([]string{"\\xA5"}))
+			Expect(mapToSequence(tokenizer.Tokenize("\\u1234"))).To(Equal([]string{"\\u1234"}))
+			Expect(
+				mapToSequence(tokenizer.Tokenize("\\U00012345\\U0006789A")),
+			).To(Equal([]string{"\\U00012345", "\\U0006789A"}))
+		})
+		Specify("continuation", func() {
+			Expect(mapToSequence(tokenizer.Tokenize("\\\n"))).To(Equal([]string{"\\\n"}))
+			Expect(mapToSequence(tokenizer.Tokenize("\\\n   "))).To(Equal([]string{"\\\n   "}))
+			Expect(mapToSequence(tokenizer.Tokenize("\\\n \t\r\f "))).To(Equal([]string{
+				"\\\n \t\r\f ",
+			}))
+			Expect(mapToSequence(tokenizer.Tokenize("\\\n \t \\\n  "))).To(Equal([]string{
+				"\\\n \t ",
+				"\\\n  ",
+			}))
+		})
+	})
+
+	Specify("incremental", func() {
+		source := "foo (bar) \\\n {$baz [sprong]}"
+		tokens := tokenizer.Tokenize(source)
+		incrementalTokens := []Token{}
+		input := NewStringStream(source)
+		tokenizer.Begin(&input)
+		for !tokenizer.End() {
+			incrementalTokens = append(incrementalTokens, *tokenizer.Next())
+		}
+		Expect(incrementalTokens).To(Equal(tokens))
+	})
+})
