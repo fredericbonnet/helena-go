75a017ae4e29ba3cd733d8fa519322a70a27b3c2
Frédéric Bonnet <frederic.bonnet@free.fr>
2024-01-28 19:43:48 +0100

    Partial conversion of TS display to Go

---
+++ src/core/display/display.go
@@ -0,0 +1,105 @@
+// Helena value display
+package display
+
+import (
+	t "helena/core/tokenizer"
+	"regexp"
+)
+
+// Value display function
+type DisplayFunction func(displayable Displayable) string
+
+// Default display function
+func DefaultDisplayFunction(displayable Displayable) string {
+	return UndisplayableValue()
+}
+
+// Undisplayable values represented as a block comment within a block.
+func UndisplayableValue() string {
+	return `{#{undisplayable value}#}`
+}
+
+// Undisplayable values represented as a label in block comment within a block.
+func UndisplayableValueWithLabel(label string) string {
+	return `{#{` + label + `}#}`
+}
+
+// Helena displayable object
+//
+// A displayable value will produce a string that, when evaluated as a word,
+// will give a value that is isomorphic with the source value.
+//
+// Undisplayable values will use a placeholder.
+type Displayable interface {
+	// Return displayable string value
+	//
+	// UnUndisplayable objects will optionally use display function fn
+	display(fn DisplayFunction) string
+}
+
+// /**
+//  * Display an object by dispatching to its `display()` function if it exists,
+//  * else use the provided function.
+//  *
+//  * @param displayable - Object to display
+//  * @param fn          - Display function for undisplayable objects
+//  *
+//  * @returns             Displayable string
+//  */
+// export function display(
+//   displayable: Displayable,
+//   fn: DisplayFunction = defaultDisplayFunction
+// ): string {
+//   return displayable.display?.(fn) ?? fn(displayable);
+// }
+
+// Return a displayable string as a single literal or as a quoted string if the
+// string contains special characters
+func DisplayLiteralOrString(str string) string {
+	tokenizer := t.Tokenizer{}
+	tokens := tokenizer.Tokenize(str)
+	if len(tokens) == 0 {
+		return `""`
+	}
+	if len(tokens) == 1 && tokens[0].Type == t.TEXT {
+		return tokens[0].Literal
+	}
+	re := regexp.MustCompile(`[\\$"({[]`)
+	return `"` + re.ReplaceAllStringFunc(str, func(c string) string { return `\` + c }) + `"`
+}
+
+// Return a displayable string as a single literal or as a block if the string
+// contains special characters
+//
+// Mostly used to display qualified value sources
+func DisplayLiteralOrBlock(str string) string {
+	tokenizer := t.Tokenizer{}
+	tokens := tokenizer.Tokenize(str)
+	if len(tokens) == 0 {
+		return `{}`
+	}
+	if len(tokens) == 1 && tokens[0].Type == t.TEXT {
+		return tokens[0].Literal
+	}
+	re := regexp.MustCompile(`[\\$"#(){}[\]]`)
+	return `{` + re.ReplaceAllStringFunc(str, func(c string) string { return `\` + c }) + `}`
+}
+
+// /**
+//  * Display a list of objects
+//  *
+//  * Useful for sentences, tuples expressions, lists...
+//  *
+//  * @param displayables - Objects to display
+//  * @param fn           - Display function for undisplayable objects
+//  *
+//  * @returns              Displayable string
+//  */
+// export function displayList(
+//   displayables: Displayable[],
+//   fn: DisplayFunction = defaultDisplayFunction
+// ): string {
+//   return `${displayables
+//     .map((displayable) => display(displayable, fn))
+//     .join(" ")}`;
+// }
---
+++ src/core/display/display_suite_test.go
@@ -0,0 +1,78 @@
+package display_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	. "helena/core/display"
+)
+
+func TestDisplay(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Display Suite")
+}
+
+// import { Script } from "./syntax";
+// import { FALSE, INT, REAL, ScriptValue, STR, TRUE } from "./values";
+
+var _ = Describe("Display", func() {
+	Describe("UndisplayableValue", func() {
+		It("should generate a placeholder block with a comment", func() {
+			Expect(UndisplayableValue()).To(Equal("{#{undisplayable value}#}"))
+		})
+		It("should accept a custom comment", func() {
+			Expect(UndisplayableValueWithLabel("some value")).To(Equal("{#{some value}#}"))
+		})
+	})
+	//
+	Describe("DisplayLiteralOrString", func() {
+		It("should generate an empty quoted string for an empty string", func() {
+			Expect(DisplayLiteralOrString("")).To(Equal(`""`))
+		})
+		It("should generate a literal for a string with no special character", func() {
+			Expect(DisplayLiteralOrString("name")).To(Equal("name"))
+			Expect(DisplayLiteralOrString("nameWithUnicode\u1234")).To(Equal("nameWithUnicode\u1234"))
+		})
+		It("should generate a quoted string for strings with special characters", func() {
+			Expect(DisplayLiteralOrString("some value")).To(Equal(`"some value"`))
+			Expect(DisplayLiteralOrString("$value")).To(Equal(`"\$value"`))
+			Expect(DisplayLiteralOrString(`\#{[()]}#"`)).To(Equal(`"\\#\{\[\()]}#\""`))
+		})
+	})
+	//
+	Describe("DisplayLiteralOrBlock", func() {
+		It("should generate an empty block for an empty string", func() {
+			Expect(DisplayLiteralOrBlock("")).To(Equal("{}"))
+		})
+		It("should generate a literal for a string with no special character", func() {
+			Expect(DisplayLiteralOrBlock("name")).To(Equal("name"))
+			Expect(DisplayLiteralOrBlock("nameWithUnicode\u1234")).To(Equal("nameWithUnicode\u1234"))
+		})
+		It("should generate a quoted string for strings with special characters", func() {
+			Expect(DisplayLiteralOrBlock("some value")).To(Equal("{some value}"))
+			Expect(DisplayLiteralOrBlock("$value")).To(Equal("{\\$value}"))
+			Expect(DisplayLiteralOrBlock(`{\#{[()]}#"}`)).To(Equal(`{\{\\\#\{\[\(\)\]\}\#\"\}}`))
+		})
+	})
+
+	//	Describe("displayList", func () {
+	//	  It("should generate a whitespace-separated list of values", func () {
+	//	    const values = [STR("literal"), STR("some string"), INT(123), REAL(1.23)];
+	//	    Expect(displayList(values)).To(Equal(`literal "some string" 123 1.23`);
+	//	  });
+	//	  It("should replace non-displayable values with placeholder", func () {
+	//	    const values = [TRUE, new ScriptValue(new Script(), undefined), FALSE];
+	//	    Expect(displayList(values)).To(Equal(
+	//	      "true {#{undisplayable value}#} false"
+	//	    );
+	//	  });
+	//	  It("should accept custom display function for non-displayable values", func () {
+	//	    const values = [TRUE, new ScriptValue(new Script(), undefined), FALSE];
+	//	    Expect(displayList(values, func () undisplayableValue("foo"))).To(Equal(
+	//	      "true {#{foo}#} false"
+	//	    );
+	//	  });
+	// })
+})
